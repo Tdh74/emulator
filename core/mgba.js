8c56142d78eaba37c4af4d9e9bebedca6585b5dd
Tdh74 <kjhilla@student.portlandk12.org>
2026-02-02 09:31:47 -0500

    Update mgba.js

--- core/mgba.js
+++ core/mgba.js
@@ -1,1 +1,27 @@
+import mGBA, { type mGBAEmulator } from '@thenick775/mgba-wasm';
+import { useEffect, useState } from 'react';
+
+export const useEmulator = (canvas: HTMLCanvasElement | null) => {
+  const [emulator, setEmulator] = useState<mGBAEmulator | null>(null);
+
+  useEffect(() => {
+    const initialize = async () => {
+      if (canvas) {
+        const Module = await mGBA({ canvas });
+
+        const mGBAVersion =
+          Module.version.projectName + ' ' + Module.version.projectVersion;
+        console.log(mGBAVersion);
+
+        await Module.FSInit();
+
+        setEmulator(Module);
+      }
+    };
+
+    initialize();
+  }, [canvas]);
+
+  return emulator;
+};
 
